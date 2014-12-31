5ac48036323b3137f2e35f699990cee1af5da37a
Wade wang <wangnmhb@live.com>
2014-12-31 11:56:04 +0800

    Crerate a Reprosory to manage my media player

---
+++ XmyMediaPlayer/XmyMediaPlayer.Controls/Window/WindowsButtonModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows;
+
+namespace XmyMediaPlayer.Controls
+{
+    internal class WindowsButtonModel : INotifyPropertyChanged
+    {
+        private Window uiWindow;
+
+        public WindowsButtonModel(Window win) {
+            this.uiWindow = win;
+            switch (win.ResizeMode)
+            {
+                case ResizeMode.CanMinimize:
+                    CanMinimize = true;
+                    CanMaximize = false;
+                    break;
+                case ResizeMode.CanResize:
+                case ResizeMode.CanResizeWithGrip:
+                    CanMinimize = true;
+                    CanMaximize = true;
+                    break;
+                case ResizeMode.NoResize:
+                    CanMinimize = false;
+                    CanMaximize = false;
+                    break;
+            }
+        }
+
+        private bool canMaximize;
+        /// <summary>
+        /// 是否可以最大化
+        /// </summary>
+        public bool CanMaximize
+        {
+            get { return canMaximize; }
+            set
+            {
+                if (canMaximize != value)
+                {
+                    canMaximize = value;
+                    NotifyPropertyChange("CanMaximize");
+                }
+            }
+        }
+
+        private bool canMinimize;
+
+        /// <summary>
+        /// 是否可以最大化
+        /// </summary>
+        public bool CanMinimize
+        {
+            get { return canMinimize; }
+            set
+            {
+                if (canMinimize != value)
+                {
+                    canMinimize = value;
+                    NotifyPropertyChange("CanMinimize");
+                }
+            }
+        }
+
+        private ICommand _maximizeCommand;
+        /// <summary>
+        /// 窗口最大化按钮事件
+        /// </summary>
+        public ICommand MaximizeCommand
+        {
+            get
+            {
+                if (_maximizeCommand == null)
+                    _maximizeCommand = new DelegateCommand(() =>
+                    {
+                        if (uiWindow == null)  return;
+                        if (uiWindow.WindowState == WindowState.Normal)
+                        {
+                            uiWindow.WindowState = WindowState.Maximized;
+                        }
+                        else
+                        {
+                            uiWindow.WindowState = WindowState.Normal;
+                        }
+                    });
+                return _maximizeCommand;
+            }
+        }
+
+        private ICommand _minimizeCommand;
+        /// <summary>
+        /// 窗口最小化按钮事件
+        /// </summary>
+        public ICommand MinimizeCommand
+        {
+            get
+            {
+                if (_minimizeCommand == null)
+                    _minimizeCommand = new DelegateCommand(() =>
+                    {
+                        if (uiWindow == null) return;
+                        uiWindow.WindowState = WindowState.Minimized;
+                    });
+                return _minimizeCommand;
+            }
+        }
+
+        private ICommand _closeCommand;
+        /// <summary>
+        /// 窗口关闭按钮事件
+        /// </summary>
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                    _closeCommand = new DelegateCommand(() =>
+                    {
+                        if (uiWindow == null)  return;
+                        uiWindow.Close();
+                    });
+                return _closeCommand;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChange(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ XmyMediaPlayer/XmyMediaPlayer/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace XmyMediaPlayer
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Window1_Loaded);
+        }
+
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream("image.png", FileMode.Create);
+                RenderTargetBitmap bmp = new RenderTargetBitmap((int)this.SystemIcon.Width, (int)this.SystemIcon.Height, 96d, 96d, PixelFormats.Pbgra32);
+                bmp.Render(this.SystemIcon);
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bmp));
+                encoder.Save(fs);
+                fs.Close();
+                fs.Dispose();
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
